7621240d6a3270fce66ebf1349d36f4c791a531e
Sven Pachnit <sven@bmonkeys.net>
2014-03-16 04:57:55 +0100

    New rails 4 app
    rails new circular_dependency -TO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CircularDependency::Application.config.secret_key_base = '1f35b4f2cc7f048164096da0333984955975e3487816b4880c3b14bcb3bd2df78985bff8e1de0168ed71da37349109215b964685faf3cc66dc14d943409ec267'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CircularDependency::Application.config.session_store :cookie_store, key: '_circular_dependency_session'
